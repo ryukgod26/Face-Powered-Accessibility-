ee575cbad2d78c9875e4012f3acabff223c2508a
ryukgod26 <ekravi26@gmail.com>
2025-09-24 11:06:15 +0000

    Trying and Playing autopygui to laern it

---
+++ mousecontrol.py
@@ -0,0 +1,14 @@
+import pyautogui
+
+def main():
+    screenWidth,screenHeight = pyautogui.size()
+    print(f'Screen Width: {screenWidth} , Screen Height: {screenHeight}')
+    pyautogui.moveTo(screenWidth/2,screenHeight/2,duration=2)
+    pyautogui.click()
+    pyautogui.click(button='right')
+    pyautogui.click(clicks=2)
+
+
+
+if __name__ == '__main__':
+    main()
