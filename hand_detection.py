3edde9bf8f14aa84f9dca4d1630b0720226165dd
raviwhitehat <ekravi26@gmail.com>
2025-09-24 23:39:56 +0530

    Solved the bugs in hand detection file and now hand detection is working as expected.(My 2 Commits were not pushing because of venv I was mistakenly tring to push venev folder)

--- hand_detection.py
+++ hand_detection.py
@@ -11,16 +11,16 @@ class handDetector:
         self.minDetectionCon = minDetectionCon
         self.minTrackingCon = minTrackingCon
         self.mpDraw = mp.solutions.drawing_utils
-        self.mphands = mp.solutiona.hands
-        self.hands = self.mphands.hands(
-                self.mode,
-                self.max_hands,
-                self.minDetectionCon,
-                self.minTrackingCon
+        self.mphands = mp.solutions.hands
+        self.hands = self.mphands.Hands(
+                static_image_mode=self.mode,
+                max_num_hands=self.max_hands,
+                min_detection_confidence=self.minDetectionCon,
+                min_tracking_confidence=self.minTrackingCon
                 )
     
     def findHands(self,img,show=True):
-        imgRGB = cv2.cvtColor(img,cv2.COLOR_BG2RGB)
+        imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
@@ -36,32 +36,33 @@ class handDetector:
     def findPosition(self,img,handNo=0,draw=True):
         lmList = []
         if self.results.multi_hand_landmarks:
-            hand = self.results.multi_hand_landmarks[handNo]
-            for id,lm in enumerate(hand.landmark):
-                ih,iw,ic = img.shape
-                cx,cy = (lm.x * iw) , (lm.y * ih)
-                lmList.append([id,cx,cy])
-                if draw:
-                    cv2.circle(
-                        img,
-                        (cx,cy),
-                        15,
-                        (0,255,255),
-                        cv2.FILLED
-                            )
+            if handNo < len(self.results.multi_hand_landmarks):
+                hand = self.results.multi_hand_landmarks[handNo]
+                for id,lm in enumerate(hand.landmark):
+                    ih,iw,ic = img.shape
+                    cx,cy = (lm.x * iw) , (lm.y * ih)
+                    lmList.append([id,cx,cy])
+                    if draw:
+                        cv2.circle(
+                            img,
+                            (cx,cy),
+                            15,
+                            (0,255,255),
+                            cv2.FILLED
+                                )
 
         return lmList
 
 
 def main():
-    cap = cv2.videoCapture()
+    cap = cv2.VideoCapture('Face_detection_testing_videos/test1.mp4')
     pTime = 0
     detector = handDetector()
     while True:
-        img,success = cap.read()
-        if not success:
-            print('Error While trying to read the video or live camera feed.')
-            sys.exit(1)
+        success,img = cap.read()
+        # if not success:
+        #     print('Error While trying to read the video or live camera feed.')
+        #     sys.exit(1)
         img = detector.findHands(img)
         lmList = detector.findPosition(img)
         if len(lmList) != 0:
@@ -69,7 +70,7 @@ def main():
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
-        cv2.putText(img,fps,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+        cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
         cv2.imshow('Hand Detection',img)
         cv2.waitKey(1)
 
